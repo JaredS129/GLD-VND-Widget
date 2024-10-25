cf9af16e06e06c94284812be5ace9408cea40319
JaredS129 <jaredkevin.stevenson@gmail.com>
2024-10-25 14:55:53 +0100

    restructure classes and improve error handling

---
+++ classes/PriceApi.py
@@ -0,0 +1,59 @@
+from typing import List
+import json
+from classes.GoldPriceData import GoldPriceData
+import requests
+
+class ApiEndpoints:
+    ALL_CURRENT_GOLD_PRICES = 'https://sjc.com.vn/GoldPrice/Services/PriceService.ashx'
+
+class ApiResponseError:
+    def __init__(self, status_code: int, error: str):
+        self.status_code = status_code
+        self.error = error
+
+    @staticmethod
+    def from_json(json_data: dict) -> 'ApiResponseError':
+        return ApiResponseError(status_code=json_data['status_code'], error=json_data['error'])
+
+class GoldPriceResponse:
+    def __init__(self, success: bool, latest_date: str, data: List[GoldPriceData]):
+        self.success = success
+        self.latest_date = latest_date
+        self.data = data
+
+    @staticmethod
+    def from_json(json_data: dict) -> 'GoldPriceResponse':
+        data = [GoldPriceData(
+            price_data_id=item['Id'],
+            type_name=item['TypeName'],
+            branch_name=item['BranchName'],
+            buy=item['Buy'],
+            buy_value=item['BuyValue'],
+            sell=item['Sell'],
+            sell_value=item['SellValue'],
+            buy_differ=item['BuyDiffer'],
+            buy_differ_value=item['BuyDifferValue'],
+            sell_differ=item['SellDiffer'],
+            sell_differ_value=item['SellDifferValue'],
+            group_date=item['GroupDate']
+        ) for item in json_data['data']]
+        return GoldPriceResponse(success=json_data['success'], latest_date=json_data['latestDate'], data=data)
+
+class PriceApi:
+    def __init__(self, endpoints: ApiEndpoints, get_all_current_gold_prices: GoldPriceResponse | ApiResponseError):
+        self.endpoints = endpoints
+        self.get_all_current_gold_prices = get_all_current_gold_prices
+
+    @staticmethod
+    def get_all_current_gold_prices() -> GoldPriceResponse | ApiResponseError:
+        response = requests.get(ApiEndpoints.ALL_CURRENT_GOLD_PRICES)
+        try:
+            parsed_response = response.json()
+        except json.decoder.JSONDecodeError:
+            return ApiResponseError(status_code=500, error='There was an error connecting to the API')
+
+        if response.status_code == 200 and parsed_response['success']:
+            json_data = parsed_response
+            return GoldPriceResponse.from_json(json_data)
+        else:
+            return ApiResponseError(status_code=500, error='There was an error connecting to the API')
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import requests
-from classes.GoldPriceResponse import GoldPriceResponse
+from classes.PriceApi import PriceApi, ApiResponseError, GoldPriceResponse
 
-response = requests.get('https://sjc.com.vn/GoldPrice/Services/PriceService.ashx')
-if response.status_code == 200:
-    json_data = response.json()
-    gold_price_response = GoldPriceResponse.from_json(json_data)
+gold_price_response = PriceApi.get_all_current_gold_prices()
 
+if isinstance(gold_price_response, GoldPriceResponse):
     print(gold_price_response.latest_date)
     for data in gold_price_response.data:
         branch_name = data.branch_name
@@ -13,6 +10,6 @@ if response.status_code == 200:
         buy = data.buy
         sell = data.sell
         print(f'{branch_name} - {type_name} - Buy: {buy} - Sell: {sell}')
-else:
-    json_data = response.json()
-    print(response.status_code, json_data['message'])
+
+if isinstance(gold_price_response, ApiResponseError):
+    print(f'{gold_price_response.status_code} Status Error: {gold_price_response.error}')
