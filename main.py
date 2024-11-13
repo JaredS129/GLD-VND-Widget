dc4c9cec7e008825bcd5fc0ddcfd89cea15d20d6
JaredS129 <jaredkevin.stevenson@gmail.com>
2024-11-13 11:40:49 +0000

    move chart code to chart class

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import matplotlib
-from PIL.ImageOps import expand
-
+from classes.PriceHistoryChart import PriceHistoryChart
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 from datetime import datetime, timedelta
 import ttkbootstrap as ttk
-from ttkbootstrap.tableview import Tableview
 from ttkbootstrap.constants import *
-import matplotlib.dates as mdates
-from classes.PriceApi import PriceApi, ApiResponseError, AllCurrentGoldPricesResponse, GoldPriceHistoryResponse
+from classes.PriceApi import PriceApi, ApiResponseError, AllCurrentGoldPricesResponse
 from utils.format_num_to_vnd_str import format_num_to_vnd_str
 
 app = ttk.Window(themename='darkly')
@@ -33,104 +28,13 @@ if isinstance(gold_price_history, ApiResponseError):
     print(gold_price_history.error)
 
 # Extract dates and prices for the line chart
-dates = [data.date for data in gold_price_history.data]
-buy_values = [data.buy_value for data in gold_price_history.data]
-sell_values = [data.sell_value for data in gold_price_history.data]
+dates: list[datetime] = [data.date for data in gold_price_history.data]
+buy_values: list = [data.buy_value for data in gold_price_history.data]
+sell_values: list = [data.sell_value for data in gold_price_history.data]
 
-# Create a figure for the line chart
-graph_frame = Figure(figsize=(7, 4), dpi=100)
-line_graph = graph_frame.add_subplot(111)
+price_history_chart = PriceHistoryChart(app, gold_price_history, colors)
+price_history_chart.build()
 
-# Plot the buy and sell values
-line_graph.plot(dates, buy_values, label='Mua vào', color=colors.danger, linewidth=2)
-line_graph.plot(dates, sell_values, label='Bán ra', color=colors.success, linewidth=2)
-
-# Format the chart
-line_graph.set_title(f"{gold_price_history.data[0].branch_name}: {gold_price_history.data[0].type_name} - 90 ngày qua", color=colors.light)
-line_graph.set_xlabel('Date', color=colors.light)
-line_graph.set_ylabel('Price', color=colors.light)
-line_graph.legend()
-
-# Remove the x and y value labels
-line_graph.set_xticklabels([])
-line_graph.set_yticklabels([])
-
-# Remove the black border
-line_graph.spines['top'].set_visible(False)
-line_graph.spines['right'].set_visible(False)
-line_graph.spines['left'].set_visible(False)
-line_graph.spines['bottom'].set_visible(False)
-
-# Remove the ticks
-line_graph.tick_params(axis='both', which='both', length=0)
-
-# Create annotations
-annotation = line_graph.annotate("", xy=(0,0), xytext=(20,20), textcoords="offset points",
-                            bbox=dict(boxstyle="round", fc="w"),
-                            arrowprops=None)
-annotation.set_visible(False)
-
-def update_annotation(x_value):
-    texts = [f"{x_value.strftime('%d/%m/%Y')}"]
-    price_to_compare: float = 0
-    diff: float = 0
-    for line in line_graph.get_lines():
-        if line.get_label() in ["Mua vào", "Bán ra"]:
-            x, y = line.get_data()
-            if x_value in x:
-                index = list(x).index(x_value)
-                price = y[index]
-                texts.append(f"{line.get_label()}: {format_num_to_vnd_str(price)}")
-                diff = abs(price_to_compare - y[index])
-                price_to_compare = y[index]
-    texts.append(f"Chênh lệch: {format_num_to_vnd_str(diff)}")
-    annotation.set_text("\n".join(texts))
-    annotation.get_bbox_patch().set_facecolor(colors.light)
-    annotation.get_bbox_patch().set_alpha(1)
-    for line in line_graph.get_lines():
-        if line.get_label().startswith("_"):
-            line.remove()
-    line_graph.axvline(x=x_value, color=colors.light, linestyle='dotted', linewidth=1, alpha=0.3)
-
-
-def hover(event):
-    vis = annotation.get_visible()
-    if event.inaxes == line_graph:
-        for line in line_graph.get_lines():
-            x, y = line.get_data()
-            if len(x) > 0 and event.xdata is not None:
-                x_datetime = mdates.num2date(event.xdata).replace(tzinfo=None)  # Convert event.xdata to naive datetime
-                x_value = min(x, key=lambda x_val: abs(x_val.replace(tzinfo=None) - x_datetime))
-                if abs(x_value - x_datetime).total_seconds() < 86400:  # Adjust the threshold as needed
-                    annotation.xy = (mdates.date2num(x_value), event.ydata)
-                    update_annotation(x_value)
-
-                    # Check if the mouse is within the rightmost quarter of the graph
-                    if event.x > 0.90 * line_graph.get_window_extent().width:
-                        annotation.set_position((-130, 20))  # Move annotation to the left side
-                    else:
-                        annotation.set_position((20, 20))  # Default position
-
-                    annotation.set_visible(True)
-                    graph_frame.canvas.draw_idle()
-                    return
-    if vis:
-        # check whether the mouse is withing the bounds of the graph
-        if event.xdata is None and event.ydata is None:
-            annotation.set_visible(False)
-        graph_frame.canvas.draw_idle()
-
-# Set the tick parameters to match the theme
-line_graph.tick_params(axis='x', colors=colors.light)
-line_graph.tick_params(axis='y', colors=colors.light)
-
-# Set the figure background color to match the theme
-graph_frame.patch.set_facecolor(colors.bg)
-line_graph.set_facecolor(colors.bg)
-
-canvas = FigureCanvasTkAgg(graph_frame, master=app)
-canvas.draw()
-canvas.get_tk_widget().pack(fill=BOTH, expand=YES)
 difference = sell_values[-1] - buy_values[-1]
 difference_label = ttk.Label(app, text=f"{format_num_to_vnd_str(difference)}", style=LIGHT, font=("Verdana", 14))
 buy_price_label = ttk.Label(app, text=f"{format_num_to_vnd_str(buy_values[-1])}", style=DANGER, font=("Verdana", 28, 'bold'))
@@ -138,7 +42,5 @@ sell_price_label = ttk.Label(app, text=f"{format_num_to_vnd_str(sell_values[-1])
 buy_price_label.pack(side=LEFT, padx=55, pady=25)
 sell_price_label.pack(side=RIGHT, padx=55, pady=25)
 difference_label.place(relx=0.5, rely=0.91, anchor='center')
-
-canvas.mpl_connect("motion_notify_event", hover)
 
 app.mainloop()
